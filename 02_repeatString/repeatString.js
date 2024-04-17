4a82be4151b4cea5ffa9fd868021c3d2bec44be0
eml <leb002@hotmail.com>
2024-04-17 13:38:50 -0300

    1st and 2nd test of 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,9 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    result = "";
+    for (let i = 0; i < num; i++) {
+        result = result + string;
+    }
+    return result;
 
 };
 
