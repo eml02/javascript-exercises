024a2a33fd4b8673d83bf1c158d11334346bd2c8
eml <leb002@hotmail.com>
2024-09-11 12:24:11 -0300

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
+    for (let index = (string.length - 1); index >= 0; index--) {
+        reversedString = reversedString + string[index];
+        console.log(string[index]);
+    }
+    return reversedString;
 
 };
+
+// let reversed = reverseString("blah");
+// console.log(reversed);
+
 
 // Do not edit below this line
 module.exports = reverseString;
