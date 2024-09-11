e10649ab69da2361da739932b17e0fb078cb1281
eml <leb002@hotmail.com>
2024-09-11 11:59:38 -0300

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < num; i++) {
+            repeatedString = repeatedString + string;
+        }
+        return repeatedString;
+    }
 };
+
+
+
+//console.log(repeatString("prueba", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
