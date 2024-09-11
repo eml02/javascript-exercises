46c593454427c2af4062a7fa4fbee777117a11f6
eml <leb002@hotmail.com>
2024-09-11 12:58:22 -0300

    Start 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value) {
+    array.forEach(element => {
+        if (element == value) {
+            array = array.toSpliced(array.indexOf(element), 1);
+        }
+    });
+    return array;
 };
+
+let array = [1,2,3,4];
+
+console.log(removeFromArray(array, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
