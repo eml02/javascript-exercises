0771a72ecd4d35d5ab4bbcae8c446c895e9a71e2
eml <leb002@hotmail.com>
2024-09-12 12:59:17 -0300

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let convertedToCelsius = (tempF - 32) * (5/9);
+  return Math.round(convertedToCelsius *10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let convertedToFahrenheit = tempC * (9/5) + 32;
+  return Math.round(convertedToFahrenheit *10)/10;
 };
+
 
 // Do not edit below this line
 module.exports = {
