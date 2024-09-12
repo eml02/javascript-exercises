4f4a6f1bb32397c1e655888723cd20b7060ba7a9
eml <leb002@hotmail.com>
2024-09-12 11:55:01 -0300

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let suma = 0;
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+    if ((Number.isInteger(num1) && Number.isInteger(num2)) && (num1 >= 0 && num2 >= 0)) {
+        for (let i = num1; i <= num2; i++) {
+            //console.log(i);
+            suma = suma + i;
+        }
+        return suma;
+    } else {
+        return "ERROR";
+    }
+    
 };
+
+console.log(sumAll(25 ,4));
 
 // Do not edit below this line
 module.exports = sumAll;
