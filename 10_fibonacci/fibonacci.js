7276d8fbc9d151fb52d4ed891cc1357bf0c8d7d2
eml <117491033+eml02@users.noreply.github.com>
2024-10-02 19:48:10 -0300

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(numero) {
+    if (typeof(numero) === "string") {
+        numero = parseInt(numero);
+    }
+    if (numero < 0) {
+        return "OOPS";
+    } else if (numero < 2) {
+        return numero;
+    } else {
+        return fibonacci(numero -1) + fibonacci(numero - 2);
+    }
 };
+
+//console.log(fibonacci(19));
 
 // Do not edit below this line
 module.exports = fibonacci;
