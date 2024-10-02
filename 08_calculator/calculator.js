b50c2bac664b803fd8994b3505272415001d6ec7
eml <117491033+eml02@users.noreply.github.com>
2024-10-02 18:30:11 -0300

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
+const add = function(n1, n2) {
+	return n1 + n2;
+};
+
+const subtract = function(n1, n2) {
+  return n1 - n2;
+};
+
+const sum = function(array) {
+  return array.reduce((sumatoria, valorActual) => sumatoria + valorActual, 0);
+};
+
+const multiply = function(array) {
+  return array.reduce((acumulador, valorActual) => acumulador * valorActual, 1);
+};
+
+const power = function(base, exp) {
+  return Math.pow(base, exp);
 	
 };
 
-const subtract = function() {
+const factorial = function(numero) {
+  if ((numero == 0) || (numero == 1)) {
+    return 1;
+  } else {
+    return factorial(numero - 1) * numero;
+  }
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
