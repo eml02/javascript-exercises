3415b5d9490768a1686ebd479e42e86d2da8798d
eml <117491033+eml02@users.noreply.github.com>
2024-10-02 19:15:18 -0300

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const allowedChara = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanedString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => allowedChara.includes(character))
+        .join('');
+    const reversedString = cleanedString.split("").reverse().join("");
+    return (cleanedString === reversedString);
+    
+
 
 };
+console.log(palindromes("som3os"));
 
 // Do not edit below this line
 module.exports = palindromes;
