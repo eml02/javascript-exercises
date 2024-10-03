560d590b620febb792ffbacd6ab84313c3d39efd
eml <117491033+eml02@users.noreply.github.com>
2024-10-02 22:49:13 -0300

    Start 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let old = people.reduce((oldest, current) => {
+        console.log(current.yearOfDeath - current.yearOfBirth);
+        oldest = current;
+        return oldest;
+    }, {})
+
 
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+
+]
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
